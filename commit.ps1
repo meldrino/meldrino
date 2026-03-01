058e0b59eb1e5c4e4ddc5fb233ab1635d3a7b38e
Andy Lear <andy@andylear.co.uk>
2026-03-01 10:54:52 +0000

    update commit script to pull before push

--- commit.ps1
+++ commit.ps1
@@ -2,5 +2,6 @@ cd C:\meldrino_app
 git add .
 $message = Read-Host "Enter commit message"
 git commit -m $message
+git pull origin main --rebase
 git push origin main
 Write-Host "Done! Changes pushed to GitHub." -ForegroundColor Green
