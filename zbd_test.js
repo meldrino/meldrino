fe516cfcd31241a0446c850d307a42a7ddd868a7
Andy Lear <andy@andylear.co.uk>
2026-03-04 13:20:46 +0000

    after getting in to zbd

---
+++ zbd_test.js
@@ -0,0 +1,30 @@
+﻿const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://api.zebedee.io/api/internal/v1/qrauth-socket');
+
+ws.on('open', () => {
+  console.log('Connected!');
+  ws.send(JSON.stringify({
+    type: 'internal-connection-sub-qr-auth',
+    data: {
+      browserOS: 'Android',
+      browserName: 'Chrome',
+      QRCodeZClient: 'browser-extension'
+    }
+  }));
+});
+
+ws.on('message', (data) => {
+  console.log('Received:', data.toString());
+});
+
+ws.on('error', (err) => {
+  console.log('Error:', err.message);
+});
+
+ws.on('close', (code, reason) => {
+  console.log('Connection closed:', code, reason.toString());
+});
+
+process.stdin.resume();
+console.log('Waiting... press Ctrl+C to exit');
