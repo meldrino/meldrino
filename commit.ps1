6e632233ef1e6c043e44f7a7d357acc7acdde97f
Andy Lear <andy@andylear.co.uk>
2026-03-10 19:59:48 +0000

    fix commit script

--- commit.ps1
+++ commit.ps1
@@ -1,10 +1,6 @@
 cd C:\meldrino_app
-git add .
+git add -A
 $message = Read-Host "Enter commit message"
-if ([string]::IsNullOrWhiteSpace($message)) {
-    Write-Host "Commit message cannot be empty. Aborting." -ForegroundColor Red
-    exit
-}
 git commit -m $message
 git push origin main
 Write-Host "Done! Changes pushed to GitHub." -ForegroundColor Green
